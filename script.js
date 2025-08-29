42326a211522acb36ae583fb756cce5bbc90d6ab
k2motorcycle <gero303@gmail.com>
2025-08-29 17:22:30 +0900

    大幅変更

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// 1. 操作したいHTML要素を取得する
+// ハンバーガーメニュー用のコード
 const hamburger = document.querySelector('.hamburger-icon');
 const navMenu = document.querySelector('.navigation-menu');
 
-// 2. ハンバーガーアイコンがクリックされたときの処理を設定する
 hamburger.addEventListener('click', () => {
-  // navMenuに 'active' というクラスを付けたり外したりする
   navMenu.classList.toggle('active');
+});
+
+// ヒーロースライダー用のコード
+const swiper = new Swiper('.hero-slider', {
+  direction: 'horizontal',
+  loop: true,
+  autoHeight: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.pagination-container .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
